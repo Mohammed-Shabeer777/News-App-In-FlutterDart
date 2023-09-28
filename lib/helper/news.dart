930e5f2ee6d70665341a1645a1b10633aac0f253
Mohammed Shabeer <Mohammedshabeer2945@gmail.com>
2023-09-28 18:41:09 +0530

    api remove

--- lib/helper/news.dart
+++ lib/helper/news.dart
@@ -1,14 +1,14 @@
 import 'dart:convert';
-
 import 'package:news_app/models/article_model.dart';
 import 'package:http/http.dart' as http;
 
 class News {
   List<ArticleModel> news = [];
-
+  //TODO: Paste api key here and one down below
+  String apikey = '';
   Future<void> getNews() async {
     String url =
-        'https://newsapi.org/v2/top-headlines?country=in&apiKey=0705cf5bba4b492ba73535316d2408ed';
+        'https://newsapi.org/v2/top-headlines?country=in&apiKey=$apikey';
     Uri uri = Uri.parse(url);
     var response = await http.get(uri);
     var jsonData = jsonDecode(response.body);
@@ -36,10 +36,11 @@ class News {
 
 class CategoryNewsClass {
   List<ArticleModel> news = [];
-
+  String apikey = '';
+  //TODO: Paste api key here as well
   Future<void> getNews({required String type}) async {
     String url =
-        'https://newsapi.org/v2/top-headlines?country=in&category=$type&apiKey=0705cf5bba4b492ba73535316d2408ed';
+        'https://newsapi.org/v2/top-headlines?country=in&category=$type&apiKey=$apikey';
     Uri uri = Uri.parse(url);
     var response = await http.get(uri);
     var jsonData = jsonDecode(response.body);
